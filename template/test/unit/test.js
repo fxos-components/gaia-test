2f475fd68454564b8c2bb6272773c20e70f1f957
Wilson Page <wilsonpage@me.com>
2014-06-23 17:48:31 +0100

    Satisfy linter

--- template/test/unit/test.js
+++ template/test/unit/test.js
@@ -1,7 +1,7 @@
+/*global suite,sinon,setup,teardown,test,assert*/
+'use strict';
 
 suite('my lib', function() {
-  'use strict';
-
   test('my test', function() {
     assert.equal(1 + 1, 2);
   });
